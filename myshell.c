9b94d2525bb71b7e87927da724030b6e632a0b5a
Spencer York <80482507+spencer-t-york@users.noreply.github.com>
2024-09-14 17:46:25 -0400

    Modularizing/Cleaning up code

--- myshell.c
+++ myshell.c
@@ -1,3 +1,4 @@
+// IMPORTS
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -7,6 +8,8 @@
 #include <sys/wait.h>
 #define MAXLINE 4096 // max length for input
 
+
+// DEFINE FUNCTIONS
 // Error Handling
 static void err_doit(int , int , const char *, va_list);
 void err_ret(const char *, ...);
@@ -15,89 +18,51 @@ void err_sys(const char *, ...);
 char *gnu_getcwd();     // return current directory
 char **split(char *);   // split shell args
 int find_pipe(char **); // find pipe character in array
+void print_prompt();    // print prompt and shows the current directory
+
+// commands
+void cd_command(char **);             // cd command
+void pipe_command(char **, int, int); // pipe command
+
 
 // MAIN FUNCTION
 int main(void) {
+    // Variables
     char   buf[MAXLINE];
     pid_t  pid;
     int    status;
 
-    printf("%s %% ", strrchr(gnu_getcwd(), '/') + 1);  // print prompt and shows the current directory
+    print_prompt();
     while (fgets(buf, MAXLINE, stdin) != NULL) {
-        if (buf[strlen(buf) - 1] == '\n')
-            buf[strlen(buf) - 1] = 0; // replace newline with null
+        if (buf[strlen(buf) - 1] == '\n') { // if the buffer has a newline at the end...
+            buf[strlen(buf) - 1] = 0;       // ...replace newline with null
+        }
 
-        char **args = split(buf);  // split buf by spaces and save to array
+        char **args = split(buf);  // split buffer by white-spaces and save to the args array
 
         // empty command
         if (args[0] == NULL) {
-            printf("%s %% ", strrchr(gnu_getcwd(), '/') + 1);
+            print_prompt();
             continue;
         }
 
         // cd command
         if (!strcmp(args[0], "cd")) { // if "cd" is entered
-            if (args[1] == NULL) {
-                fprintf(stderr, "cd: missing argument\n"); // Handle missing argument
-            } else if (chdir(args[1]) != 0) {
-                perror("cd"); // Print error if chdir fails
-            }
-            printf("%s %% ", strrchr(gnu_getcwd(), '/') + 1);
+            cd_command(args);         // ...call cd command
             continue;
         }
 
         // pipe command
         int const pipe_pos = find_pipe(args);
         if (pipe_pos != -1) {
-            args[pipe_pos] = NULL; // set the pipe = NULL
-            char **args_l = args, **args_r = &args[pipe_pos+1]; // split args left and right of pipe
-            pid_t pid1, pid2;
-            int p[2];
-
-            if (pipe(p) < 0) {
-                perror("pipe");
-                exit(1);
-            }
-
-            // left side
-            if ((pid1 = fork()) < 0) {
-                err_sys("fork error");
-            } else if (pid1 == 0) {
-                close(p[0]);                // clear pipe read data
-                close(STDOUT_FILENO);    // close fd 1 so it's the lowest fd open
-                dup(p[1]);                  // write pipe output (this goes to the lowest fd open)
-                close(p[1]);                // clear pipe write data
-
-                execvp(args_l[0], args_l);  // execute left side
-                err_ret("couldn't execute: %s", args_l[0]); // if execvp fails, return error
-                _exit(127);
-            }
-
-            // right side
-            if ((pid2 = fork()) < 0) {
-                err_sys("fork error");
-            } else if (pid2 == 0) {
-                close(p[1]);               // clear pipe write data
-                close(STDIN_FILENO);    // close fd 0 so it's the lowest fd open
-                dup(p[0]);                 // reads pipe input (this goes to the lowest fd open)
-                close(p[0]);               // clear pipe read data
-
-                execvp(args_r[0], args_r);  // execute right side
-                err_ret("couldn't execute: %s", args_r[0]); // if execvp fails, return error
-                _exit(127);
-            }
-
-            close(p[0]);  // close both ends in parent
-            close(p[1]);
-
-            if (waitpid(pid1, &status, 0) < 0)
-                err_sys("waitpid error");
-
-            if (waitpid(pid2, &status, 0) < 0)
-                err_sys("waitpid error");
-
-            printf("%s %% ", strrchr(gnu_getcwd(), '/') + 1);
+            pipe_command(args, pipe_pos, status);
             continue;
+        }
+
+        // quit command
+        if (strcmp(args[0], "quit") == 0) { // if "quit" is entered...
+            printf("exiting...\n");
+            break; // break out of loop
         }
 
         if ((pid = fork()) < 0) {  // if fork() fails
@@ -106,33 +71,23 @@ int main(void) {
 
         /* ----- CHILD PROCESS ----- */
         else if (pid == 0) {
-            if (!strcmp(args[0], "quit")) {      // if "quit" is entered
-                _exit(0);                             // exit child process
-            } else {
-                execvp(args[0], args); // replace process with specifed process in args[]
-                err_ret("couldn't execute: %s", buf); // if execvp fails, return error
-                _exit(127);
-            }
+            // TODO: change execvp to execv
+            execvp(args[0], args); // replace process with specifed process in args[]
+            err_ret("couldn't execute: %s", buf); // if execvp fails, return error
+            _exit(127);
+        } else {
+            if ((pid = waitpid(pid, &status, 0)) < 0)
+                err_sys("waitpid error");
+            print_prompt();
         }
-
-        /* ----- PARENT PROCESS ----- */
-        if (strcmp(args[0], "quit") == 0) { // if "quit" was entered, exit parent process
-            printf("exiting...\n");
-            break; // break out of loop
-        }
-
-        if ((pid = waitpid(pid, &status, 0)) < 0)
-            err_sys("waitpid error");
-        // display shell prompt again with current directory
-        printf("%s %% ", strrchr(gnu_getcwd(), '/') + 1);
     }
     exit(0);
 }
 
 
-// ---------- FUNCTIONS ---------- //
+// ------------------------------ FUNCTIONS ------------------------------ //
 
-// ERROR HANDLING
+// ------ ERROR HANDLING ------ //
 static void err_doit(int errnoflag, int error, const char *fmt, va_list ap) {
     char buf[MAXLINE];
     vsnprintf(buf, MAXLINE-1, fmt, ap);
@@ -143,14 +98,12 @@ static void err_doit(int errnoflag, int error, const char *fmt, va_list ap) {
     fputs(buf, stderr);
     fflush(NULL);      /* flushes all stdio output streams */
 }
-
 void err_ret(const char *fmt, ...) {
     va_list   ap;
     va_start(ap, fmt);
     err_doit(1, errno, fmt, ap);
     va_end(ap);
 }
-
 void err_sys(const char *fmt, ...) {
     va_list   ap;
     va_start(ap, fmt);
@@ -159,7 +112,7 @@ void err_sys(const char *fmt, ...) {
     exit(1);
 }
 
-// RETURN CURRENT DIRECTORY FUNCTION
+// ------ RETURN CURRENT DIRECTORY ------ //
 // Function adapted from GNU C Library manual:
 // https://www.gnu.org/software/libc/manual/html_node/Working-Directory.html
 char *gnu_getcwd ()
@@ -178,7 +131,7 @@ char *gnu_getcwd ()
     }
 }
 
-// SPLIT BUFFER BY WHITESPACE FUNCTION
+// ------ SPLIT BUFFER BY WHITESPACE FUNCTION ------ //
 char **split(char *str) {
     char **newArray = (char **)malloc(MAXLINE * sizeof(char *));
     char *token = strtok(str, " ");
@@ -193,7 +146,7 @@ char **split(char *str) {
     return newArray;
 }
 
-// FUNCTION TO FIND LOCATION OF PIPE
+// ------ FIND LOCATION OF PIPE ------ //
 int find_pipe(char **args) {
     for (int i = 0; args[i] != NULL; i++) {
         if (strcmp(args[i], "|") == 0) {
@@ -201,4 +154,71 @@ int find_pipe(char **args) {
         }
     }
     return -1;
+}
+
+void print_prompt() {
+    printf("%s %% ", strrchr(gnu_getcwd(), '/') + 1);
+}
+
+// ------ CD COMMAND ------ //
+void cd_command(char **args) {
+    if (args[1] == NULL) {
+        fprintf(stderr, "cd: missing argument\n"); // Handle missing argument
+    } else if (chdir(args[1]) != 0) {
+        perror("cd"); // Print error if chdir() fails
+    }
+    print_prompt(); // Print prompt after handling cd command
+}
+
+// ------ PIPE COMMAND ------ //
+void pipe_command(char **args, int pipe_pos, int status) {
+    args[pipe_pos] = NULL; // set the pipe = NULL
+    char **args_l = args, **args_r = &args[pipe_pos+1]; // split args left and right of pipe
+    pid_t pid1, pid2;
+    int p[2];
+
+    if (pipe(p) < 0) {
+        perror("pipe");
+        exit(1);
+    }
+
+    // left side
+    if ((pid1 = fork()) < 0) {
+        err_sys("fork error");
+    } else if (pid1 == 0) {
+        close(p[0]);                // clear pipe read data
+        close(STDOUT_FILENO);    // close fd 1 so it's the lowest fd open
+        dup(p[1]);                  // write pipe output (this goes to the lowest fd open)
+        close(p[1]);                // clear pipe write data
+        // TODO: change execvp to execv
+        execvp(args_l[0], args_l);  // execute left side
+        err_ret("couldn't execute: %s", args_l[0]); // if execvp fails, return error
+        _exit(127);
+    }
+
+    // right side
+    if ((pid2 = fork()) < 0) {
+        err_sys("fork error");
+    } else if (pid2 == 0) {
+        close(p[1]);               // clear pipe write data
+        close(STDIN_FILENO);    // close fd 0 so it's the lowest fd open
+        dup(p[0]);                 // reads pipe input (this goes to the lowest fd open)
+        close(p[0]);               // clear pipe read data
+
+        // TODO: change execvp to execv
+        execvp(args_r[0], args_r);  // execute right side
+        err_ret("couldn't execute: %s", args_r[0]); // if execvp fails, return error
+        _exit(127);
+    }
+
+    close(p[0]);  // close both ends in parent
+    close(p[1]);
+
+    if (waitpid(pid1, &status, 0) < 0)
+        err_sys("waitpid error");
+
+    if (waitpid(pid2, &status, 0) < 0)
+        err_sys("waitpid error");
+
+    print_prompt();
 }
